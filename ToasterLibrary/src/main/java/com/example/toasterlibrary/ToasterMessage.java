c028dc99c3d2326a968da5fd9e3361d596dea951
ziggy1971 <extremevb@gmail.com>
2021-12-01 09:21:09 -0600

    New comments

--- ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
+++ ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
@@ -8,7 +8,5 @@ public class ToasterMessage {
     public static void s(Context c, String message) {
         // Create a new Toast message
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
-        
-//        Toast
     }
 }

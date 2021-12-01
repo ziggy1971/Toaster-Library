97c89fa9d07c52bb1396e1079b11ca8b68c98a05
ziggy1971 <extremevb@gmail.com>
2021-12-01 09:21:35 -0600

    New comments

--- ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
+++ ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
@@ -6,7 +6,7 @@ import android.widget.Toast;
 public class ToasterMessage {
     
     public static void s(Context c, String message) {
-        // Create a new Toast message
+        // Create a new Toast messages
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
     }
 }
